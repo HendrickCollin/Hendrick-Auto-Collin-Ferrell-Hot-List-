ff47d3f3f9427695f9be4b3d469170b5543d71a9
HendrickCollin <collin.ferrell@hendrickauto.com>
2026-05-01 18:06:32 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HotList from './HotList.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HotList />
+  </React.StrictMode>
+);
